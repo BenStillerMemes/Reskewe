666681a57d3e6cee4de797b5f093e36bacbec53c
LukeLaupheimer <makhai@gmail.com>
2017-04-17 16:36:11 -0400

    Fix merge errors

--- Assets/AnimationTest.cs
+++ Assets/AnimationTest.cs
@@ -24,10 +24,10 @@ public class AnimationTest : MonoBehaviour {
 
 		if (Input.GetKey ("d") && transform.localScale.x != 1) 
 		{
-			transform.localScale = new Vector3 (1, 1, 1);
+			transform.localScale = new Vector3 (1.5f, 1.5f, 1);
 		} else if (Input.GetKey ("a") && transform.localScale.x != -1) 
 		{
-				transform.localScale = new Vector3 (1.5f, 1.5f, 1);
+				transform.localScale = new Vector3 (-1.5f, 1.5f, 1);
 			walkAnim.SetBool ("walking", true);
 		} else if (Input.GetKeyDown ("a")) 
 		{
